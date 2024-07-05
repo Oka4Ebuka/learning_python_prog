bad11a1e14edb16b0602b5f55f1d373759fc9e0a
okafor emmanuel <okaformanuel97@gmail.com>
2024-07-05 02:36:54 +0100

    code slices

--- chapter_2/marvin.py
+++ chapter_2/marvin.py
@@ -2,4 +2,14 @@ paranoidAndroid = "marvin"
 letters = list(paranoidAndroid)
 for char in letters:
     print('\t', char)
-    print('\t' *2, char)
+
+paranoidAndroid = "marvin, the paranoid android"
+letters = list(paranoidAndroid)
+for char in letters[:6]:
+    print('\t', char)
+print()
+for char in letters[-7:]:
+    print('\t'*2, char)
+print()
+for char in letters[12:20]:
+    print('\t'*3, char)
