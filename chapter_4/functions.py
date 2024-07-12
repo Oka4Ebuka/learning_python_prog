641879ec0972f1a845e1f0e4a82c28e252d58fce
okafor emmanuel <okaformanuel97@gmail.com>
2024-07-12 03:02:26 +0100

    fourth commit

--- chapter_4/functions.py
+++
@@ -1,7 +0,0 @@
-def search4vowels():
-    vowels = set('aeiou')
-    word = input("provide a word to search for vowels")
-    found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
-        
