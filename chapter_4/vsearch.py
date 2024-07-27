481f21c47fea67b76f137be8639eda96b158b07e
okafor emmanuel <okaformanuel97@gmail.com>
2024-07-27 17:36:29 +0100

    invoking functions

--- chapter_4/vsearch.py
+++ chapter_4/vsearch.py
@@ -7,10 +7,19 @@ def search4vowels():
         print(vowel)
 search4vowels()
 
+#A function is FALSE if it evaluates to zero, the value NONE, an empty string or an empty data structure
 bool(0)
 bool(0.0)
 bool('')
 bool([])
 bool({})
 bool(None)
-print(bool(None))
+
+#A function is TRUE for every non-empty list, string or data structure
+bool(1)
+bool(-1)
+bool(42)
+bool(0.0000000001)
+bool('panic')
+bool([42, 43, 44])
+bool({'a': 42, 'b': 43})
