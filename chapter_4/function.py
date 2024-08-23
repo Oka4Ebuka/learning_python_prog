0faf3448ddf3edb3d5fdec134e5aa22685f7e27d
okafor emmanuel <okaformanuel97@gmail.com>
2024-08-23 16:38:45 +0100

    creating a function

---
+++ chapter_4/function.py
@@ -0,0 +1,20 @@
+ #print('emmanuel okafor')
+
+def name_identifier(passed_name):
+    """display name"""
+    print(passed_name)
+
+def return_name(passed_name):
+    """returns name"""
+    return passed_name
+
+#variable declaration
+name_1 = 'emmanuel okafor'
+name_2 = 'rita'
+passed_name = 'scott'
+
+#call to our function
+name_identifier(passed_name)
+#x = return_name(name_2)
+#print(x)
+#print(return_name(name_1))
